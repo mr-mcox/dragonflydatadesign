5ef4f9b59ed0d3e4bdea380037640e04b9eaec0a
Matthew Cox <matthew.cox@teachforamerica.org>
2016-11-10 19:58:40 -0600

    Getting basic site up with bootstrap (whew!)

---
+++ manage.py
@@ -0,0 +1,13 @@
+from flask_script import Manager
+from app import create_app
+
+app = create_app()
+
+manager = Manager(app)
+
+@manager.command
+def runserver():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    manager.run()
--- tests/test_functional_navigation.py
+++ tests/test_functional_navigation.py
@@ -2,19 +2,24 @@ import pytest
 from selenium import webdriver
 from flask import url_for
 
+@pytest.mark.usefixtures('live_server')
+class TestLiveServer:
 
-@pytest.fixture(scope='class')
-def browser(request):
-    browser = webdriver.Firefox()
+    @pytest.fixture(scope='class')
+    def browser(self, request):
+        browser = webdriver.Firefox()
 
-    def quit_browser():
-        browser.quit()
+        def quit_browser():
+            browser.quit()
 
-    request.addfinalizer(quit_browser)
-    return browser
+        request.addfinalizer(quit_browser)
+        return browser
 
+    # @pytest.mark.xfail
 
-def test_selenium():
-    driver = webdriver.Firefox()
-    driver.get("http://www.python.org")
-    assert "Python" in driver.title
+    @pytest.mark.usefixtures('live_server')
+    def test_navigate_to_projects(self, browser, app):
+        # When user navigates to main page
+        browser.get(url_for('main.index', _external=True))
+        # Then they get a page
+        assert 'Dragonfly Data' in browser.title
