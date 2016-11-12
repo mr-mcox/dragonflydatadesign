31317bea351097127d396d1d17860f7a8dd1d2c9
Matthew Cox <matthew.cox@teachforamerica.org>
2016-11-12 10:08:21 -0600

    I think that I fixed the in-stublime-text build issue

--- tests/test_project_robin.py
+++ tests/test_project_robin.py
@@ -1,7 +1,9 @@
 import json
 from flask import url_for
+import sys
 
 def test_get_data(client, app):
     resp = client.get(url_for('main.get_pr_data'))
     data = json.loads(resp.data.decode('utf-8'))
+    print(sys.path)
     assert type(data['cms']) is list
