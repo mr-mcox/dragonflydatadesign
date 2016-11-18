e886a7ee23687a09fc5e6153bbca51970358988b
Matthew Cox <matthew.cox@teachforamerica.org>
2016-11-17 18:54:25 -0600

    Basic portfolio page work

--- app/main/portfolio.py
+++ app/main/portfolio.py
@@ -1,7 +1,9 @@
 class Portfolio(object):
+
     """Managing portfolio data"""
-    def __init__(self):
-        pass
+
+    def __init__(self, data=None):
+        self.data = data
 
     def get_page_content(self, portfolio_item, page):
-        pass
+        return self.data[portfolio_item-1][page-1]
--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -1,6 +1,7 @@
 import json
 from flask import url_for
 from unittest.mock import patch
+from app.main.portfolio import Portfolio
 
 
 def test_get_data(client, app):
@@ -13,15 +14,28 @@ def test_get_portfolio_page(client, app):
     resp = client.get(url_for('main.portfolio_page', id=1))
     assert b'Project Robin' in resp.data
 
-# Generate page from mocked get page content
-
 
 def test_page_from_content(client, app):
     with patch('app.main.portfolio.Portfolio.get_page_content') as mock_get_content:
-        rv = {'title': 'The title', 'copy':'Some text'}
+        rv = {'title': 'The title', 'copy': 'Some text', 'picture': 'img.png'}
         mock_get_content.return_value = rv
         resp = client.get(url_for('main.portfolio_details', id=1, page=1))
         assert b'The title' in resp.data
         assert b'Some text' in resp.data
+        assert bytes(
+            url_for('static', filename=rv['picture']), 'utf-8') in resp.data
 
-# Return content from passed page and portfolio item
+
+def test_get_page_content():
+    p_in = [
+        [{'title': 'Some title', 'copy': 'Some text', 'picture': 'img.png'}],
+        [
+            {'title': 'The title', 'copy': 'Some text', 'picture': 'img.png'},
+            {'title': 'The title 2', 'copy': 'Some text',
+                'picture': 'img.png'},
+        ]
+    ]
+    p = Portfolio(p_in)
+    assert p.get_page_content(portfolio_item=1, page=1) == p_in[0][0]
+    assert p.get_page_content(portfolio_item=2, page=1) == p_in[1][0]
+    assert p.get_page_content(portfolio_item=2, page=2) == p_in[1][1]
